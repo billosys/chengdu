68ac6444876dbfc3cc8a9f34a617c95821bfaaef
Daniel Fiser <danfis@danfis.cz>
2018-03-11 14:35:03 +0100

    Added pddl-check program.

--- bin/check.c
+++ bin/check.c
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[])
 {
+    pddl_config_t cfg = PDDL_CONFIG_INIT;
     pddl_t *pddl;
 
     if (argc != 3){
@@ -10,12 +11,22 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    pddl = pddlNew(argv[1], argv[2]);
-    if (pddl == NULL)
+    pddlErrSetWarnOutput(stderr);
+    pddlErrSetInfoOutput(stderr);
+    cfg.force_adl = 0;
+    cfg.normalize = 1;
+    cfg.strips = 1;
+    cfg.strips_cfg.prune.enable = 0;
+    cfg.strips_cfg.h_mutex = 0;
+    cfg.strips_cfg.fa_mgroup = 0;
+    cfg.fdr = 0;
+
+    pddl = pddlNew(argv[1], argv[2], &cfg);
+    if (pddl == NULL){
+        pddlErrPrintWithTraceback();
         return -1;
+    }
 
-    pddlDump(pddl, stdout);
     pddlDel(pddl);
-
     return 0;
 }
