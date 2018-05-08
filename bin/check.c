aa451bdb81eeb07ecfb0f899a35d40c92d928da0
Daniel Fiser <danfis@danfis.cz>
2018-05-08 16:26:54 +0200

    Cleaned bin/

--- bin/check.c
+++ bin/check.c
@@ -4,29 +4,30 @@
 int main(int argc, char *argv[])
 {
     pddl_config_t cfg = PDDL_CONFIG_INIT;
-    pddl_t *pddl;
+    pddl_strips_config_t strips_cfg = PDDL_STRIPS_CONFIG_INIT;
+    pddl_t pddl;
+    pddl_strips_t *strips;
 
     if (argc != 3){
-        fprintf(stderr, "Usage; %s domain.pddl problem.pddl\n", argv[0]);
+        fprintf(stderr, "Usage: %s domain.pddl problem.pddl\n", argv[0]);
         return -1;
     }
 
     pddlErrSetWarnOutput(stderr);
     pddlErrSetInfoOutput(stderr);
-    cfg.force_adl = 0;
-    cfg.normalize = 1;
-    cfg.strips = 1;
-    cfg.strips_cfg.prune.enable = 0;
-    cfg.strips_cfg.h_mutex = 0;
-    cfg.strips_cfg.fa_mgroup = 0;
-    cfg.fdr = 0;
-
-    pddl = pddlNew(argv[1], argv[2], &cfg);
-    if (pddl == NULL){
+    cfg.force_adl = 0; // TODO: parametrize
+    if (pddlInit(&pddl, argv[1], argv[2], &cfg) != 0){
         pddlErrPrintWithTraceback();
         return -1;
     }
 
-    pddlDel(pddl);
+    strips = pddlStripsNew(&pddl, &strips_cfg);
+    if (strips == NULL){
+        pddlErrPrintWithTraceback();
+        return -1;
+    }
+
+    pddlStripsDel(strips);
+    pddlFree(&pddl);
     return 0;
 }
