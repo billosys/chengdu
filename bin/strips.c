3d2be03bd83c336939672d6167f987edb703472c
Daniel Fiser <danfis@danfis.cz>
2018-05-27 10:52:16 +0200

    Added bin/pddl-strips program.

---
+++ bin/strips.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pddl/pddl.h>
+
+int main(int argc, char *argv[])
+{
+    pddl_config_t cfg = PDDL_CONFIG_INIT;
+    pddl_strips_config_t strips_cfg = PDDL_STRIPS_CONFIG_INIT;
+    pddl_t pddl;
+    pddl_strips_t *strips;
+
+    if (argc != 3){
+        fprintf(stderr, "Usage: %s domain.pddl problem.pddl\n", argv[0]);
+        return -1;
+    }
+
+    pddlErrSetWarnOutput(stderr);
+    pddlErrSetInfoOutput(stderr);
+    cfg.force_adl = 0; // TODO: parametrize
+    if (pddlInit(&pddl, argv[1], argv[2], &cfg) != 0){
+        pddlErrPrintWithTraceback();
+        return -1;
+    }
+
+    pddlNormalize(&pddl);
+    strips = pddlStripsNew(&pddl, &strips_cfg);
+    if (strips == NULL){
+        pddlErrPrintWithTraceback();
+        return -1;
+    }
+
+    pddlStripsPrintDebug(strips, stdout);
+
+    pddlStripsDel(strips);
+    pddlFree(&pddl);
+    return 0;
+}
+
