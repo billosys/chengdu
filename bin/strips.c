4836e53b5c260702c548ae2b461380e343ccf04a
Daniel Fiser <danfis@danfis.cz>
2018-06-08 16:12:15 +0200

    Fixed bin/ for the new API.

--- bin/strips.c
+++ bin/strips.c
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[])
 {
     pddl_config_t cfg = PDDL_CONFIG_INIT;
-    pddl_strips_config_t strips_cfg = PDDL_STRIPS_CONFIG_INIT;
+    pddl_ground_config_t ground_cfg = PDDL_GROUND_CONFIG_INIT;
     pddl_t pddl;
     bor_err_t err = BOR_ERR_INIT;
-    pddl_strips_t *strips;
+    pddl_strips_t strips;
 
     if (argc != 3){
         fprintf(stderr, "Usage: %s domain.pddl problem.pddl\n", argv[0]);
@@ -16,22 +16,21 @@ int main(int argc, char *argv[])
 
     borErrWarnEnable(&err, stderr);
     borErrInfoEnable(&err, stderr);
-    cfg.force_adl = 0; // TODO: parametrize
+    cfg.force_adl = 1; // TODO: parametrize
     if (pddlInit(&pddl, argv[1], argv[2], &cfg, &err) != 0){
         borErrPrint(&err, 1, stderr);
         return -1;
     }
 
     pddlNormalize(&pddl);
-    strips = pddlStripsNew(&pddl, &strips_cfg, &err);
-    if (strips == NULL){
+    if (pddlStripsGround(&strips, &pddl, &ground_cfg, &err) != 0){
         borErrPrint(&err, 1, stderr);
         return -1;
     }
 
-    pddlStripsPrintDebug(strips, stdout);
+    pddlStripsPrintDebug(&strips, stdout);
 
-    pddlStripsDel(strips);
+    pddlStripsFree(&strips);
     pddlFree(&pddl);
     return 0;
 }
