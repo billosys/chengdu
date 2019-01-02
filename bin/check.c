c11ec31feae687e12b11a27bf72e35843fd38183
Daniel Fiser <danfis@danfis.cz>
2019-01-02 16:08:33 +0100

    bin/pddl-check: Added some options and added some useful info to stdout.

--- bin/check.c
+++ bin/check.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <pddl/pddl.h>
+#include <opts.h>
+
+struct options {
+    int help;
+    int quiet;
+    int force_adl;
+    int compile_away_cond_eff;
+    int compile_away_cond_eff_pddl;
+} o;
 
 int main(int argc, char *argv[])
 {
@@ -9,23 +18,88 @@ int main(int argc, char *argv[])
     bor_err_t err = BOR_ERR_INIT;
     pddl_strips_t strips;
 
-    if (argc != 3){
-        fprintf(stderr, "Usage: %s domain.pddl problem.pddl\n", argv[0]);
+    bzero(&o, sizeof(o));
+    optsAddDesc("help", 'h', OPTS_NONE, &o.help, NULL,
+                "Print this help.");
+    optsAddDesc("quiet", 'q', OPTS_NONE, &o.quiet, NULL,
+                "Disable logging and error output.");
+    optsAddDesc("adl", 'a', OPTS_NONE, &o.force_adl, NULL,
+                "Force :adl requirement even if it is not specified in the"
+                " domain file.");
+    optsAddDesc("ce", 0x0, OPTS_NONE, &o.compile_away_cond_eff, NULL,
+                "Compile away conditional effects on the STRIPS level"
+                " (recommended).");
+    optsAddDesc("ce-pddl", 0x0, OPTS_NONE, &o.compile_away_cond_eff_pddl, NULL,
+                "Compile away conditional effects on the PDDL level.");
+
+    if (opts(&argc, argv) || o.help || argc != 3){
+        fprintf(stderr, "Usage: %s [OPTIONS] domain.pddl problem.pddl\n",
+                argv[0]);
+        fprintf(stderr, "  OPTIONS:\n");
+        optsPrint(stderr, "    ");
+        fprintf(stderr, "\n");
         return -1;
     }
 
-    borErrWarnEnable(&err, stderr);
-    borErrInfoEnable(&err, stderr);
-    cfg.force_adl = 0; // TODO: parametrize
+    if (o.quiet){
+        borErrWarnEnable(&err, NULL);
+        borErrInfoEnable(&err, NULL);
+    }else{
+        borErrWarnEnable(&err, stderr);
+        borErrInfoEnable(&err, stdout);
+    }
+
+    cfg.force_adl = 0;
+    if (o.force_adl)
+        cfg.force_adl = 1;
+
     if (pddlInit(&pddl, argv[1], argv[2], &cfg, &err) != 0){
-        borErrPrint(&err, 1, stderr);
+        if (!o.quiet){
+            fprintf(stderr, "Error: ");
+            borErrPrint(&err, 1, stderr);
+        }
         return -1;
     }
 
     pddlNormalize(&pddl);
+    if (o.compile_away_cond_eff_pddl)
+        pddlCompileAwayCondEff(&pddl);
+
+    if (!o.quiet){
+        BOR_INFO(&err, "Number of PDDL Types: %d", pddl.type.size);
+        BOR_INFO(&err, "Number of PDDL Objects: %d", pddl.obj.size);
+        BOR_INFO(&err, "Number of PDDL Predicates: %d", pddl.pred.size);
+        BOR_INFO(&err, "Number of PDDL Functions: %d", pddl.func.size);
+        BOR_INFO(&err, "Number of PDDL Actions: %d", pddl.action.size);
+        BOR_INFO(&err, "Number of PDDL Metric: %d", pddl.metric);
+    }
+
     if (pddlStripsGround(&strips, &pddl, &ground_cfg, &err) != 0){
-        borErrPrint(&err, 1, stderr);
+        pddlFree(&pddl);
+        if (!o.quiet){
+            fprintf(stderr, "Error: ");
+            borErrPrint(&err, 1, stderr);
+        }
         return -1;
+    }
+
+    if (o.compile_away_cond_eff)
+        pddlStripsCompileAwayCondEff(&strips);
+
+    if (!o.quiet){
+        BOR_INFO(&err, "Number of Strips Operators: %d", strips.op.op_size);
+        BOR_INFO(&err, "Number of Strips Facts: %d", strips.fact.fact_size);
+
+        int count = 0;
+        for (int i = 0; i < strips.op.op_size; ++i){
+            if (strips.op.op[i]->cond_eff_size > 0)
+                ++count;
+        }
+        BOR_INFO(&err, "Number of Strips Operators"
+                       " with Conditional Effects: %d", count);
+        BOR_INFO(&err, "Goal is unreachable: %d",
+                 strips.goal_is_unreachable);
+        BOR_INFO(&err, "Has Conditional Effects: %d", strips.has_cond_eff);
     }
 
     pddlStripsFree(&strips);
